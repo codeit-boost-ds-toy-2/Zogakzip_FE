58f5b930f54b30c1e1bc32e8a30a46260e419b81
pookey1104 <pookey1104@naver.com>
2024-10-11 03:01:26 +0900

    ✨ Feat : 검색바 (#3)
    검색창 구현, 카테고리에 따라 placeholder 변경, 반응형 반영

---
+++ src/styles/components/input/InputSearchStyle.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import palette from "../../../lib/colorPalette";
+
+export const InputSearch = styled.div`
+  width: calc(100% - 374px - 40px);
+  height: calc(45px - 20px);
+  border-radius: 6px;
+  background: ${palette.gray100};
+
+  padding: 10px 20px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+export const SearchImg = styled.img`
+  width: 24px;
+  cursor: pointer;
+`;
+
+export const InputText = styled.input`
+  width: 100%;
+  font-family: "Spoqa Han Sans Neo", "sans-serif";
+  font-size: 14px;
+  font-weight: 400;
+  letter-spacing: -0.42px;
+  border: none;
+  outline: none;
+  background: ${palette.gray100};
+`;
