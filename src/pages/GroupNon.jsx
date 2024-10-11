03b3272322b1c5fde551fa2a354293ccacfa0b6e
pookey1104 <pookey1104@naver.com>
2024-10-12 07:55:05 +0900

    ✨ Feat : 비공개 그룹 접근 권환 확인 페이지 (#7)
    비공개 그룹 접근 권환 확인 페이지 구현

--- src/pages/GroupNon.jsx
+++ src/pages/GroupNon.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import * as G from "../styles/GroupNonStyle";
 
+import Header from "../components/Header";
+import InputText from "../components/input/InputText";
+import SizeLBtn from "../components/SizeLBtn";
+
 function GroupNon() {
+  const handleGroupClick = () => {
+    console.log("비공개 그룹 접근 권환 확인 페이지 API 연동 예정");
+  };
+
   return (
     <>
-      <G.GroupNon>그룹 접근 권한 페이지</G.GroupNon>
+      <G.GroupNon>
+        <Header showSizeMBtn={false} />
+        <G.Center>
+          <G.Title>비공개 그룹</G.Title>
+          <G.Text>비공개 그룹에 접근하기 위해 권한 확인이 필요합니다.</G.Text>
+          <InputText label="비밀번호 입력" placeholder="그룹 비밀번호를 입력해 주세요" category="password" />
+          <SizeLBtn text="제출하기" onClick={handleGroupClick} />
+        </G.Center>
+      </G.GroupNon>
     </>
   );
 }
