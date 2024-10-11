7a26f7f89f1144098cee0d485356ee73e85dbc15
pookey1104 <pookey1104@naver.com>
2024-10-12 06:29:08 +0900

    💡 Add : 추억 올리기 페이지 추가 (#7)
    누락된 추억 올리기 페이지 추가

---
+++ src/pages/PostCreate.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as P from "../styles/PostCreateStyle";
+
+function PostCreate() {
+  return (
+    <>
+      <P.PostCreate>추억 올리기 페이지</P.PostCreate>
+    </>
+  );
+}
+
+export default PostCreate;
