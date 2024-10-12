6ed94b18859dc17278a7fc00aa3d0e79582f4d8d
pookey1104 <pookey1104@naver.com>
2024-10-12 11:01:01 +0900

    ✨ Feat : 추억 수정 모달 구현 및 추억 line 길이 수정 (#7)
    추억 수정 모달 구현 및 추억 올리기 페이지 Line 높이 유동적으로 수정

--- src/styles/components/modal/MemoryModModalStyle.js
+++ src/styles/components/modal/MemoryModModalStyle.js
@@ -1,20 +1,64 @@
 import styled from "styled-components";
-import palette from "../lib/colorPalette";
+import palette from "../../../lib/colorPalette";
 
-export const PostCreate = styled.div`
+export const ModalSpace = styled.div`
+  width: 100vw;
+  height: 100dvh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const ModalBackground = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalWrap = styled.div`
+  width: calc(1120px - 80px);
+  padding: 40px;
+  border-radius: 6px;
+  z-index: 2;
+  background-color: ${palette.gray50};
+
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 60px;
-  padding-bottom: 60px;
 `;
 
-export const Title = styled.div`
+export const ModalTitle = styled.div`
   color: ${palette.black};
   font-size: 24px;
   font-weight: 700;
   letter-spacing: -0.72px;
+`;
+
+export const ModalCloseSpace = styled.div`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  justify-content: flex-end;
   margin-top: -20px;
+  margin-right: -20px;
+  margin-bottom: -70px;
+`;
+
+export const ModalClose = styled.img`
+  width: 30px;
+  cursor: pointer;
 `;
 
 export const FormBox = styled.div`
@@ -60,13 +104,4 @@ export const Toggle = styled.div`
   font-size: 14px;
   font-weight: 400;
   letter-spacing: -0.42px;
-`;
-
-export const CloseBtn = styled.img`
-  width: 34px;
-  cursor: pointer;
-
-  position: absolute;
-  top: 122px;
-  left: calc(50vw + 480px + 60px);
 `;
